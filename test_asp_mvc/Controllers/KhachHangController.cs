2b9a369792a89373e1d086a2e6424f32fd9f8670
Nhat Le <symbol057vn@gmail.com>
2023-05-29 14:44:49 +0700

    LUYEN TAP build chuc nang theem khach hang voi radio check box tum lum

---
+++ test_asp_mvc/Controllers/KhachHangController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test_asp_mvc.Models;
+
+namespace test_asp_mvc.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        public ActionResult DanhSach()
+        {
+            return View(DanhSachKhachHang.dsKhachHang);
+        }
+
+        public ActionResult ThemMoi()
+        {
+            return View(new KhachHang() {ID = 0 });
+        }
+        [HttpPost]
+        public ActionResult Themmoi(KhachHang thongsokhach)
+        {
+            if(thongsokhach.ID == 0)
+            {
+                ModelState.AddModelError("", "ID phai nhap > 0");
+                return View(thongsokhach);
+            }
+                DanhSachKhachHang.dsKhachHang.Add(thongsokhach);
+            return RedirectToAction("DanhSach");
+        }
+
+    }
+}
---
+++ test_asp_mvc/Models/KhachHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_asp_mvc.Models
+{
+    public class KhachHang
+    {
+        public int ID { set; get; }
+        public string TenKhachHang { set; get; }
+        public string SoDienThoai { set; get; }
+        public string DiaChiNhanHang { set; get; }
+        public string Email { set; get; }
+        public string GioiTinh { set; get; }
+    }
+}
